ac8be8de18d0f03ca08fcf5be4ca835e3bbc6d96
bitcoinxag <30301695+bitcoinxag@users.noreply.github.com>
2017-11-21 16:40:50 -0600

    Official Source
    BitcoinDiamond
    Decentralised P2P

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitcoinDiamond-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitcoinDiamondURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
